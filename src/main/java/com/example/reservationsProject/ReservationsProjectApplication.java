2051b727b3b41c775f2b34303835ef0629778312
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-11-10 11:29:54 +0100

    creato progetto

---
+++ src/main/java/com/example/reservationsProject/ReservationsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservationsProject/ReservationsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
